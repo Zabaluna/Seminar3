767998e17d62cccc6a57b912c4c85a73d4df4f75
Наталья Ярцева <zabaluna@mail.ru>
2022-10-22 22:56:09 +0300

    Length between 2 points 2D

---
+++ Task21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите X первой точки ");
+int userXa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y первой точки ");
+int userYa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите X второй точки ");
+int userXb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y второй точки ");
+int userYb = Convert.ToInt32(Console.ReadLine());
+
+double L = Math.Sqrt(Math.Pow((userXa - userXb),2) + (Math.Pow((userYa - userYb),2)));
+Console.WriteLine($"Расстояние между точками равно {L}");
