ea47ad90f3378416e68766cc4868d167d0796d24
Наталья Ярцева <zabaluna@mail.ru>
2022-10-27 00:36:51 +0300

    function

--- HomeWorkTask21/Program.cs
+++ HomeWorkTask21/Program.cs
@@ -3,6 +3,13 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+double distance(int userXa, int userYa, int userZa, int userXb, int userYb, int userZb)
+{
+    double LengthPoints = 0;
+    LengthPoints = Math.Sqrt(Math.Pow((userXa - userXb), 2) + (Math.Pow((userYa - userYb), 2) + (Math.Pow((userZa - userZb), 2))));
+    return LengthPoints;
+}
+
 Console.WriteLine("Введите X первой точки ");
 int userXa = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Y первой точки ");
@@ -10,16 +17,17 @@ int userYa = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Y первой точки ");
 int userZa = Convert.ToInt32(Console.ReadLine());
 
-
-
 Console.WriteLine("Введите X второй точки ");
 int userXb = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Y второй точки ");
 int userYb = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Y второй точки ");
+Console.WriteLine("Введите Z второй точки ");
 int userZb = Convert.ToInt32(Console.ReadLine());
 
+double dist = distance(userXa, userYa, userZa, userXb, userYb, userZb);
 
-double LengthPoints = Math.Sqrt(Math.Pow((userXa - userXb),2) + (Math.Pow((userYa - userYb),2) + (Math.Pow((userZa - userZb),2))));
-// Console.WriteLine($"Расстояние между точками равно {LengthPoints}");
-Console.WriteLine($"Расстояние между точками равно {Math.Round(LengthPoints, 2, MidpointRounding.AwayFromZero)}");
+// double LengthPoints = Math.Sqrt(Math.Pow((userXa - userXb),2) + (Math.Pow((userYa - userYb),2) + (Math.Pow((userZa - userZb),2))));
+
+// // Console.WriteLine($"Расстояние между точками равно {LengthPoints}");
+Console.WriteLine($"Расстояние между точками равно {Math.Round(dist, 2, MidpointRounding.AwayFromZero)}");
+
