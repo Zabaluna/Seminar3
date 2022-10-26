d25c7a42072e3558202829ae1f90b4cba5ecc650
Наталья Ярцева <zabaluna@mail.ru>
2022-10-27 00:05:24 +0300

    indents and brackets

--- HomeWorkTask19/Program.cs
+++ HomeWorkTask19/Program.cs
@@ -8,21 +8,23 @@ int Number = Convert.ToInt32(Console.ReadLine());
 
 if (Number > 9999 && Number < 100000)
 {
- int Number1 = Number/10000; 
- int Number5 = Number % 10;
+    int Number1 = Number / 10000;
+    int Number5 = Number % 10;
 
- int Number2 = (Number / 1000) % 10;
- int Number4 =(Number / 10) % 10;
+    int Number2 = (Number / 1000) % 10;
+    int Number4 = (Number / 10) % 10;
 
- if ( Number2 == Number4 && Number1 == Number5 )
-    Console.WriteLine("Введенное число является палиндромом ");
- else
- {
-    Console.WriteLine("Введенное число НЕ является палиндромом ");
- }
+    if (Number2 == Number4 && Number1 == Number5)
+    {
+        Console.WriteLine("Введенное число является палиндромом ");
+    }
+    else
+    {
+        Console.WriteLine("Введенное число НЕ является палиндромом ");
+    }
 }
-else 
-  Console.WriteLine("Вы ввели не пятизначное число");
+else
+    Console.WriteLine("Вы ввели не пятизначное число");
 
 // Console.WriteLine($"{Number2},{Number4}");
 // Console.WriteLine($"{Number1},{Number5}");
