dc943e6f3c1912f70ec0d6ed16830a4107f14d2b
Наталья Ярцева <zabaluna@mail.ru>
2022-10-28 01:16:57 +0300

    Fuction added

--- HomeWorkTask23/Program.cs
+++ HomeWorkTask23/Program.cs
@@ -3,20 +3,35 @@
 // 3 -> 1, 8, 27 
 // 5 -> 1, 8, 27, 64, 125
 
+int Cube(int numbN);
+
+for (int i = 1; i < numbN; i++)
+{
+    Console.Write(Math.Pow(i, 3) + " ");
+}
+
 Console.WriteLine("Введите число ");
 int numbN = Convert.ToInt32(Console.ReadLine());
-
-if (numbN <= 0)
-{
-    Console.WriteLine("Ваше число меньше нуля");
-}
-else
-{
-    for (int i = 1; i < numbN; i++)
-    {
-        Console.Write(Math.Pow(i, 3) + ", ");
-    }
-    Console.WriteLine($"{Math.Pow(numbN, 3)}, ");
-}
+Cube(numbN);
 
 
+//  int Cube(int numbN);
+// {
+//     if (numbN > 0)
+//     {
+//         int i = 1;
+//         while (i <= numberN)
+//         {
+//             Console.Write(Math.Pow(i, 3) + " ");
+//             i++;
+//         }
+//     }
+//     else
+//     {
+//             Console.WriteLine(" Введите число от 1");
+//     }
+// }
+
+// Console.WriteLine("Введите число ");
+// int numbN = Convert.ToInt32(Console.ReadLine());
+// Cube(numbN);
