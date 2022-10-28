23f61517637e8ae11607db7c1b8773c09e1d0839
Наталья Ярцева <zabaluna@mail.ru>
2022-10-28 03:02:56 +0300

    Function and reduction of operation

--- HomeWorkTask19/Program.cs
+++ HomeWorkTask19/Program.cs
@@ -8,13 +8,7 @@ void Palindrome(int Number)
 {
     if (Number > 9999 && Number < 100000)
     {
-        int Number1 = Number / 10000;
-        int Number5 = Number % 10;
-
-        int Number2 = (Number / 1000) % 10;
-        int Number4 = (Number / 10) % 10;
-
-        if (Number2 == Number4 && Number1 == Number5)
+        if ((Number / 1000) % 10 == (Number / 10) % 10 && Number / 10000 == Number % 10)
         {
             Console.WriteLine("Введенное число является палиндромом ");
         }
@@ -31,5 +25,9 @@ Console.WriteLine("Введите пятизначное число ");
 int Number = Convert.ToInt32(Console.ReadLine());
 Palindrome(Number);
 
+// int Number1 = Number / 10000;
+// int Number5 = Number % 10;
+// int Number2 = (Number / 1000) % 10;
+// int Number4 = (Number / 10) % 10;
 // Console.WriteLine($"{Number2},{Number4}");
 // Console.WriteLine($"{Number1},{Number5}");
